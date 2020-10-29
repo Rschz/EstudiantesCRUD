7e96cd3ac17c755aec4fc6918dc6ebd6fd92fbce
Rschz <radeluis.shanchez@gmail.com>
2020-10-28 23:11:50 -0400

    cambios en el diseño del formulario

--- includes/a_config.php
+++ includes/a_config.php
@@ -2,7 +2,7 @@
 	switch (basename($_SERVER["SCRIPT_NAME"])) {
 		case "student_form.php":
 			$CURRENT_PAGE = "student_form"; 
-			$PAGE_TITLE = "Nuevo Estudiante";
+			$PAGE_TITLE = "Agregar estudiante";
 			break;
 		default:
 			$CURRENT_PAGE = "Index";
--- student_form.php
+++ student_form.php
@@ -11,49 +11,57 @@
 
 	<div class="container mb-5">
 		<div class="d-block mb-3">
-		<a class="btn btn-primary" href="index.php" role="button">Volver</a>
+			<a class="btn btn-primary" href="index.php" role="button">Volver</a>
 		</div>
-		<h2 class="text-center">Gestion de contactos</h2>
-        <form class="needs-validation" novalidate>
-            <div class="form-row">
-              <div class="col-md-6 mb-3">
-                <label for="nombre">Nombre*</label>
-                <input type="text" class="form-control" id="nombre" placeholder="Introducir nombre..." required>
-                <div class="invalid-feedback">
-                  Introducir un nombre valido
-                </div>
-              </div>
-              <div class="col-md-6 mb-3">
-                <label for="apellido">Apellido*</label>
-                <input type="text" class="form-control" id="apellido" placeholder="Introducir apellido..." required>
-                <div class="invalid-feedback">
-                  Introducir un apellido valido.
-                </div>
-              </div>
-            </div>
-            <div class="form-row">
-              <div class="col-md-6 mb-3">
-                <label for="telefono">Telefono*</label>
-                <input type="number" class="form-control" id="telefono" placeholder="8095798556" required>
-                <div class="invalid-feedback">
-                  Introducir un apellido valido.
-                </div>
-              </div>
-              <div class="col-md-3 mb-3">
-                <label for="tipo">Tipo*</label>
-                <select class="custom-select" id="tipo" required>
-                  <option selected disabled value="">Elegir...</option>
-                  <option>Laboral</option>
-                  <option>Personal</option>
-                  <option>Academico</option>
-                </select>
-                <div class="invalid-feedback">
-                  Introduce un apellido valido.
-                </div>
-              </div>
-            </div>
-            <div> <button class="btn btn-block btn-primary" type="submit">Guardar</button> <button id="cancelar" name="btnCancelar" class="btn btn-block btn-danger" type="button">Cancelar</button></div>
-          </form>
+		<h2 class="text-center"><?= $PAGE_TITLE;?></h2>
+		<form>
+			<div class="form-group">
+				<label for="Nombre">Nombre *</label>
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<div class="input-group-text">
+							<i class="fa fa-user"></i>
+						</div>
+					</div>
+					<input id="Nombre" name="Nombre" placeholder="Escribir aquí..." type="text" required="required" class="form-control">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="apellido">Apellido *</label>
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<div class="input-group-text">
+							<i class="fa fa-user-o"></i>
+						</div>
+					</div>
+					<input id="apellido" name="apellido" placeholder="Escribir aquí..." type="text" class="form-control" required="required">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="carrera">Carrera *</label>
+				<div>
+					<select id="carrera" name="carrera" class="custom-select" required="required">
+						<option value="0" readonly>Seleccione una carrera...</option>
+						<option value="1">Redes</option>
+						<option value="2">Software</option>
+						<option value="3">Multimedia</option>
+						<option value="4">Mecatronica</option>
+						<option value="5">Seguridad informática</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<div>
+					<div class="custom-control custom-checkbox custom-control-inline">
+						<input name="isActive" id="isActive_0" type="checkbox" class="custom-control-input" value="si">
+						<label for="isActive_0" class="custom-control-label">Esta activo</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<button name="submit" type="submit" class="btn btn-primary">Agregar</button>
+			</div>
+		</form>
 	</div>
 
 	<?php include("includes/footer.php"); ?>

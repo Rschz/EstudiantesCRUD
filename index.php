ef526e75cc847836b86741fb3fc7369844c62784
Rschz <radeluis.shanchez@gmail.com>
2020-10-29 00:27:03 -0400

    Cambio en el diseño de los botones

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
 
 	<div class="container">
 		<div class="d-block mb-3">
-		<a class="btn btn-primary" href="student_form.php" role="button">Agregar Nuevo</a>
+			<a class="btn btn-outline-primary" href="student_form.php" role="button"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+				</svg> Agregar Nuevo</a>
 		</div>
 		<table class="table table-borderless table-hover">
 			<thead class="thead-dark">
@@ -29,7 +31,18 @@
 					<td>Mark</td>
 					<td>8095557546</td>
 					<td>Personal</td>
-					<td class="text-center"><button id="editar" name="btnEditar" class="btn btn-warning col-4">Editar</button> <button id="eliminar" name="btnEliminar" class="btn btn-danger col-4 elim">Eliminar</button></td>
+					<td class="text-center">
+						<button id="editar" name="btnEditar" class="btn btn-warning col-4">
+							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+							</svg>
+						</button>
+						<button id="eliminar" name="btnEliminar" class="btn btn-danger col-4 elim" title="Eliminar">
+							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z" />
+							</svg>
+						</button>
+					</td>
 				</tr>
 				<tr class="border-bottom">
 					<td>2</td>
--- student_form.php
+++ student_form.php
@@ -11,16 +11,20 @@
 
 	<div class="container mb-5">
 		<div class="d-block mb-3">
-			<a class="btn btn-primary" href="index.php" role="button">Volver</a>
+			<a class="btn btn-link" href="index.php" role="button"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-90deg-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z" />
+				</svg> Volver</a>
 		</div>
-		<h2 class="text-center"><?= $PAGE_TITLE;?></h2>
+		<h2 class="text-center"><?= $PAGE_TITLE; ?></h2>
 		<form>
 			<div class="form-group">
 				<label for="Nombre">Nombre *</label>
 				<div class="input-group">
 					<div class="input-group-prepend">
 						<div class="input-group-text">
-							<i class="fa fa-user"></i>
+							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+							</svg>
 						</div>
 					</div>
 					<input id="Nombre" name="Nombre" placeholder="Escribir aquí..." type="text" required="required" class="form-control">
@@ -31,7 +35,9 @@
 				<div class="input-group">
 					<div class="input-group-prepend">
 						<div class="input-group-text">
-							<i class="fa fa-user-o"></i>
+							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+							</svg>
 						</div>
 					</div>
 					<input id="apellido" name="apellido" placeholder="Escribir aquí..." type="text" class="form-control" required="required">
@@ -41,12 +47,12 @@
 				<label for="carrera">Carrera *</label>
 				<div>
 					<select id="carrera" name="carrera" class="custom-select" required="required">
-						<option value="0" readonly>Seleccione una carrera...</option>
-						<option value="1">Redes</option>
-						<option value="2">Software</option>
-						<option value="3">Multimedia</option>
-						<option value="4">Mecatronica</option>
-						<option value="5">Seguridad informática</option>
+						<option value="" selected disabled>Seleccione una carrera...</option>
+						<option value="0">Redes</option>
+						<option value="1">Software</option>
+						<option value="2">Multimedia</option>
+						<option value="3">Mecatronica</option>
+						<option value="4">Seguridad informática</option>
 					</select>
 				</div>
 			</div>
